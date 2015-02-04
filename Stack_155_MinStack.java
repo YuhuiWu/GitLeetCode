dfffdd195473b56ea2498d0e15cf8ea877c932dc
Yuhui Wu <yuhuiwu@buffalo.edu>
2015-02-03 22:49:50 -0500

    155 Min Stack

---
+++ Stack_155_MinStack.java
@@ -0,0 +1,32 @@
+﻿/**
+ *Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ *
+ *push(x) -- Push element x onto stack.
+ *pop() -- Removes the element on top of the stack.
+ *top() -- Get the top element.
+ *getMin() -- Retrieve the minimum element in the stack.
+ */
+class Stack_155_MinStack {
+    private Stack<Integer> stack=new Stack<Integer>();
+    private Stack<Integer> minStack=new Stack<Integer>();
+    public void push(int x) {
+        if(minStack.isEmpty()||x<=minStack.peek()){
+            minStack.push(x);
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        if(stack.peek().equals(minStack.peek()))
+            minStack.pop();
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
