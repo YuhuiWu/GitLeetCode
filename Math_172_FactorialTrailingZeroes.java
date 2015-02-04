b0ff97e3539a6ee2971a8f60ca03120566b9275d
Yuhui Wu <yuhuiwu@buffalo.edu>
2015-02-03 22:19:33 -0500

    172 Factorial Trailing Zeros

---
+++ Math_172_FactorialTrailingZeroes.java
@@ -0,0 +1,17 @@
+﻿/**
+ *Given an integer n, return the number of trailing zeroes in n!.
+ *
+ *Note: Your solution should be in logarithmic time complexity.
+ */
+public class Math_172_FactorialTrailingZeroes {
+	public int trailingZeroes(int n) {
+		int ret=0;
+		while(true){
+			n=n/5;
+      ret+=n;
+      if(n==0){
+      	break;
+      }  		
+    }
+    return ret;
+}
